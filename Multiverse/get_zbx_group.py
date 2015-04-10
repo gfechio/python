4e59e4dce244c49f98579010da678ee5b8c165b6
Giordano Fechio <gfechio@L-BRZH71RFH0-M.local>
2015-04-10 17:36:46 -0300

    Multivers Project fix

--- Multiverse/get_zbx_group.py
+++ Multiverse/get_zbx_group.py
@@ -4,6 +4,9 @@ import json
 import multiverse_connect as m
 
 def get_group(token, hostname):
+    hostgroup = []
     hostid = json.loads(m.connect({"jsonrpc": "2.0","method": "host.get","params": {"output": "extend","filter": {"host": [ hostname ]}},"auth": token,"id":"1"}))['result'][0]['hostid']
-    hostgroup = json.loads(m.connect({"jsonrpc": "2.0", "method": "hostgroup.get", "params": {"output": "extend", "hostids": hostid},"auth": token,"id":"1"}))['result'][0]['name']  
+    raw_hostgroup = json.loads(m.connect({"jsonrpc": "2.0", "method": "hostgroup.get", "params": {"output": "extend", "hostids": hostid},"auth": token,"id":"1"}))['result']
+    for host_group in raw_hostgroup:
+        hostgroup.append(host_group.get('name'))
     return hostgroup
