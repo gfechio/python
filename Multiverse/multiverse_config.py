153f9cae69e9fc9b8a7606a47d5a75d956aa4340
Giordano Fechio <gfechio@L-BRZH71RFH0-M.local>
2015-04-10 12:16:15 -0300

    Multiverse Project

---
+++ Multiverse/multiverse_config.py
@@ -0,0 +1,10 @@
+connect = dict(
+    zbx_server = "http://monitoracao.vmcommerce.intra/zabbix/api_jsonrpc.php",
+    spw_server = "http://napsao-nix-qa-pci-manager1.qa.vmcommerce.intra/rpc/api"
+)
+login = dict(
+    zbx_user = "svc_prod_zabbix_adm",
+    zbx_password = "SVCwm123!",
+    spw_user = "admin",
+    spw_password = "services123456"
+)
